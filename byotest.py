5047e71199ea80c6752f6e903dc674a93058709f
Giselle <gigi108@gmail.com>
2018-07-17 07:58:55 +0000

    completed assertions for evenofevens

--- byotest.py
+++ byotest.py
@@ -2,3 +2,11 @@
 
 def assert_equal(a,b):
     assert a == b, "Expected {0} and {1} to be the same, but they were different".format(a, b)
+    
+    
+def assert_true(a):
+    assert a, "not true"
+    
+def assert_false(a):
+    assert not a, "not false"
+    
--- testevens.py
+++ testevens.py
@@ -1,8 +1,12 @@
+from byotest import *
+
 
 def even_number_of_evens(numbers):
     number_of_evens = 0
     
     for n in numbers:
+        if type(n) != int:
+            continue
         if n % 2 == 0:
             number_of_evens += 1
     
@@ -11,12 +15,24 @@ def even_number_of_evens(numbers):
     
     return number_of_evens % 2 == 0
     
+# assert_equal(even_number_of_evens([2,3,4]), True)
+# assert_equal(even_number_of_evens([2,3,5]), False)
+# assert_equal(even_number_of_evens([1,3]), False)
+# assert_equal(even_number_of_evens([]), False)
 
-assert even_number_of_evens([2, 3, 4]) == True, "2 Even numbers" 
-assert even_number_of_evens([2, 3, 5]) == False, "1 Even Number"
-assert even_number_of_evens([1, 3]) == False, "No Even Numbers"
-assert even_number_of_evens([]) == False, "No Numbers"
+assert_true(even_number_of_evens([2,3,4]))
+assert_false(even_number_of_evens([2,3,5]))
+assert_false(even_number_of_evens([1,3]))
+assert_false(even_number_of_evens([]))
+assert_false(even_number_of_evens(["Hello", "World"]))
+assert_false(even_number_of_evens(["Hello", "World", "Hi"]))
 
-#assert(even_number_of_evens(["Hello", "World"]) == False)
+
+# assert_even_number_of_evens([2, 3, 4]) == True, "2 Even numbers" 
+# assert even_number_of_evens([2, 3, 5]) == False, "1 Even Number"
+# assert even_number_of_evens([1, 3]) == False, "No Even Numbers"
+# assert even_number_of_evens([]) == False, "No Numbers"
+
+
 
 print("OK")
